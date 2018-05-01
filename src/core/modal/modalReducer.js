1fcdaa48adab70c6e0489725a98da6f049e42112
Ben Honeywill <benhoneywill@gmail.com>
2018-05-01 13:28:17 +0100

    fix modal import

--- src/core/modal/modalReducer.js
+++ src/core/modal/modalReducer.js
@@ -1,8 +1,8 @@
-import { OPEN_MODAL, CLOSE_MODAL } from "./dashboardConstants";
+import { OPEN_MODAL, CLOSE_MODAL } from "./modalConstants";
 
 const defaultState = null;
 
-export default function dashboardReducer(state = defaultState, action) {
+export default function modalReducer(state = defaultState, action) {
   switch (action.type) {
     case OPEN_MODAL:
       return action.modal;
